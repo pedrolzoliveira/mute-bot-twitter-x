e6e9ff5f1227c307d8722ffc1535f765ac88fe72
Pedro Oliveira <pluiz@mychirotouch.com>
2024-06-15 13:12:02 -0300

    add print when server is started

--- server/main.go
+++ server/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"mutebotx/server"
 	"net/http"
 )
 
 func main() {
 	server := server.CreateServer()
+	fmt.Println("Server started on port 8080")
 	http.ListenAndServe(":8080", server)
 }
