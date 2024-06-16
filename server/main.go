55d1c29269a0d10fd6b6c060ccca9bec902c8b57
Pedro Oliveira <pluiz@mychirotouch.com>
2024-06-15 23:38:52 -0300

    get port from env variable

--- server/main.go
+++ server/main.go
@@ -4,10 +4,17 @@ import (
 	"fmt"
 	"mutebotx/server"
 	"net/http"
+	"os"
 )
 
 func main() {
 	server := server.CreateServer()
-	fmt.Println("Server started on port 8080")
-	http.ListenAndServe(":8080", server)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("Server started on port " + port)
+	http.ListenAndServe(":"+port, server)
 }
